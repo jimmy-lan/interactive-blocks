18dc02a2674bef55bd0a6391a4ab8853786f087e
Jimmy <jimmylanbcn@gmail.com>
2020-11-23 13:05:00 -0500

    Add AttributeRegistry

---
+++ src/models/common/AttributeRegistry.ts
@@ -0,0 +1,44 @@
+/**
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-23
+ * Description: Class holding attributes regarding a model.
+ */
+
+class AttributeRegistry<T> {
+  constructor(private data: T) {}
+
+  /**
+   * Get data corresponding to <key> from attributes.
+   * To get all data, use getAll.
+   * @param key key used to get property.
+   * @see getAll
+   */
+  get = <K extends keyof T>(key: K): T[K] => {
+    return this.data[key];
+  };
+
+  getAll = (): T => {
+    return this.data;
+  };
+
+  /**
+   * Set <newData> to attributes. Entries not included in
+   * <newData> but in the previous state will be persisted.
+   * Use replace function for data replacement.
+   * @param newData
+   * @see replace
+   */
+  set = (newData: T): void => {
+    Object.assign(this.data, newData);
+  };
+
+  /**
+   * Replace with <newData>. If you would like some entries
+   * from the previous state to be persisted, use set function.
+   * @param newData
+   * @see set
+   */
+  replace = (newData: T): void => {};
+}
+
+export { AttributeRegistry };
