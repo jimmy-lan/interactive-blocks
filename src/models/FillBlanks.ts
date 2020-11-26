9ed11b5aede675429c09217a4f6d29bc7d20a8c6
Jimmy <jimmylanbcn@gmail.com>
2020-11-25 23:52:34 -0500

    Add validateAttributes function

--- src/models/FillBlanks.ts
+++ src/models/FillBlanks.ts
@@ -10,7 +10,17 @@ export interface FillBlanksProps extends QuestionProps {
    * A list of correct answers. If the user's response is included in this list,
    * the user is considered to answer the question correctly.
    */
-  acceptableAnswers: string[];
+  acceptableAnswers?: string[];
+  /**
+   * A boolean value deciding whether to hint the user on the number of
+   * characters in the correct answer. If this option is provided and
+   * set to a number (or true), all answers in <acceptableAnswers> must have
+   * the same length, or <acceptableAnswers> should only contain one string.
+   * If you are checking answers using <getAnswer> function, make sure
+   * to return a list of string in the required format. Otherwise, the
+   * feature will fail to work.
+   */
+  hintNumChars?: boolean | number;
   /**
    * Callback function to obtain a list of option <id> which corresponds to
    * the correct answer. When this attribute is not specified, the
@@ -26,5 +36,52 @@ export class FillBlanks extends Question<FillBlanksProps> {
       new AttributeRegistry<FillBlanksProps>(attributes),
       persistenceStorage
     );
+    this.validateAttributes();
   }
+
+  /**
+   * Check validity of attributes in this question model.
+   * Can change the current attributes so that the meaning of them stay the same.
+   */
+  private validateAttributes = (): void => {
+    const { acceptableAnswers, hintNumChars, getAnswer } = this.getAll();
+
+    if (!acceptableAnswers && !getAnswer) {
+      throw new Error(
+        "FillBlanks must contain one of the attributes: 'acceptableAnswers' | 'getAnswer'"
+      );
+    }
+
+    if (
+      acceptableAnswers !== undefined &&
+      !getAnswer &&
+      !acceptableAnswers.length
+    ) {
+      console.warn(
+        `No answer to FillBlank question '${this.get(
+          "id"
+        )}' will be marked correct.`
+      );
+    }
+
+    if (hintNumChars !== undefined && acceptableAnswers !== undefined) {
+      if (!acceptableAnswers.length) {
+        throw new Error(
+          `At lease one element in 'acceptableAnswers' of FillBlank question '${this.get(
+            "id"
+          )}' ` + `should exist.`
+        );
+      }
+      const acceptableAnswerLength = acceptableAnswers[0].length;
+      acceptableAnswers.forEach((acceptableAnswer: string) => {
+        if (acceptableAnswer.length !== acceptableAnswerLength) {
+          throw new Error(
+            "If attribute 'hintNumChars' is provided and " +
+              "set to a number (or true), all answers in 'acceptableAnswers' must have " +
+              "the same length, or 'acceptableAnswers' should only contain one string."
+          );
+        }
+      });
+    }
+  };
 }
