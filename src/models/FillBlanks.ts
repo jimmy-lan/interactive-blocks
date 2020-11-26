0629b09692a367860d5b361a4ebb4d7f7a4ea217
Jimmy <jimmylanbcn@gmail.com>
2020-11-25 21:36:41 -0500

    Implement FillBlanks.ts

--- src/models/FillBlanks.ts
+++ src/models/FillBlanks.ts
@@ -2,6 +2,29 @@
  * Created by Jimmy Lan
  * Creation Date: 2020-11-25
  */
-import { QuestionProps } from "./Question";
+import { Question, QuestionProps } from "./Question";
+import { AttributeRegistry } from "./common/AttributeRegistry";
 
-export interface FillBlanksProps extends QuestionProps {}
+export interface FillBlanksProps extends QuestionProps {
+  /**
+   * A list of correct answers. If the user's response is included in this list,
+   * the user is considered to answer the question correctly.
+   */
+  acceptableAnswers: string[];
+  /**
+   * Callback function to obtain a list of option <id> which corresponds to
+   * the correct answer. When this attribute is not specified, the
+   * `acceptableAnswers` property is used. Otherwise, `acceptableAnswers is
+   * ignored.
+   */
+  getAnswer?: () => Promise<string[]>;
+}
+
+export class FillBlanks extends Question<FillBlanksProps> {
+  constructor(attributes: FillBlanksProps, persistenceStorage?: Storage) {
+    super(
+      new AttributeRegistry<FillBlanksProps>(attributes),
+      persistenceStorage
+    );
+  }
+}
