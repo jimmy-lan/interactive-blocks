3ef963f762524d8d73bc4ba1a8646f53aecd0d45
Jimmy <jimmylanbcn@gmail.com>
2020-11-25 21:37:51 -0500

    Add FillBlanksComponent.ts

---
+++ src/views/FillBlanks/FillBlanksComponent.ts
@@ -0,0 +1,12 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-25
+ */
+
+import { QuestionContainer } from "../common/QuestionContainer";
+import { FillBlanks, FillBlanksProps } from "../../models/FillBlanks";
+
+export class FillBlanksComponent extends QuestionContainer<
+  FillBlanks,
+  FillBlanksProps
+> {}
