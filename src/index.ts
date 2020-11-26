ff17054f17a9c04789646f960fb5e0e4bbf81e5c
Jimmy <jimmylanbcn@gmail.com>
2020-11-26 14:17:47 -0500

    Update global window type declaration

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-23
+ */
+
+// scripts
+import * as models from "./models";
+import * as views from "./views";
+
+// stylesheets
+import "./styles/main.css";
+
+const Blocks = { ...models, ...views };
+
+declare global {
+  interface Window {
+    Blocks: typeof Blocks;
+  }
+}
+
+window.Blocks = Blocks;
+
+export default Blocks;
