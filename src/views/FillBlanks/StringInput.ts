7b31ddf715ff4f37639dc390e2ef63887f86488c
Jimmy <jimmylanbcn@gmail.com>
2020-11-26 10:59:59 -0500

    Update attribute validation

--- src/views/FillBlanks/StringInput.ts
+++ src/views/FillBlanks/StringInput.ts
@@ -8,7 +8,11 @@ import { FillBlanks, FillBlanksProps } from "../../models";
 
 export class StringInput extends BlockComponent<FillBlanks, FillBlanksProps> {
   get htmlStructure(): string {
+    // Information needed to render
+    const hintNumChars = this.model.get("hintNumChars");
+
     // Styles require calculation
+    let style = "";
 
     return `
       <input class="ib-fb-input" style="width: calc(1.5ch * ${9})" maxlength="${9}" value="forty two" />
