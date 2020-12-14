ec7dcabcad3becfd4b1924175d0204291c5dbd2e
Jimmy <jimmylanbcn@gmail.com>
2020-12-14 18:24:04 -0500

    Add dev example

---
+++ examples/dev-example/index.js
@@ -0,0 +1,18 @@
+const { MultipleChoice, MultipleChoiceComponent } = Blocks;
+
+const options = MultipleChoice.parseOptions(
+  ["useCSC309", "useUofT", "useEffect", "useComponent"],
+  [2]
+);
+const mc = new MultipleChoice({
+  id: "multiple-choice-react",
+  question: "Which of the following is a valid hook in React?",
+  options,
+  allowMultipleSelect: true,
+});
+
+const div = document.getElementById("root");
+
+console.log(div);
+
+new MultipleChoiceComponent(div, mc).render();
