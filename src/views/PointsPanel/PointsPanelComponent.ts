96907d0b5528d1f88989b0c3dd893aaf063b9e91
Jimmy <jimmylanbcn@gmail.com>
2020-12-15 15:29:53 -0500

    Add structure to PointsPanelComponent

--- src/views/PointsPanel/PointsPanelComponent.ts
+++ src/views/PointsPanel/PointsPanelComponent.ts
@@ -3,13 +3,17 @@
  * Creation Date: 2020-12-15
  */
 import { BlockComponent } from "../common/BlockComponent";
-import { PointsPanel, PointsPanelProps } from "../../models/PointsPanel";
+import { PointsPanel, PointsPanelProps } from "../../models";
 
 export class PointsPanelComponent extends BlockComponent<
   PointsPanel,
   PointsPanelProps
 > {
   get htmlStructure(): string {
-    return "";
+    return `
+      <div class="ib-points-panel">
+        <h5>1 / 2</h5>
+      </div>
+    `;
   }
 }
