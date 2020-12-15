b27652e6f519a61bdc50cafe1047c2f8a01b7e44
Jimmy <jimmylanbcn@gmail.com>
2020-12-15 18:14:17 -0500

    Add CollectionComponent.ts

---
+++ src/views/common/CollectionComponent.ts
@@ -0,0 +1,37 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-12-15
+ * Description: A view containing a collection of components.
+ */
+
+import { Component } from "./Component";
+import { BlockCollection, BlockModel } from "../../models/common";
+
+export abstract class CollectionComponent<T extends BlockModel<K>, K>
+  implements Component {
+  constructor(public parent: Element, public collection: BlockCollection<T>) {}
+
+  /**
+   * Render one component based on the model given.
+   * Append the component rendered to the parent element.
+   * @param model Model to render
+   */
+  abstract appendComponent(model: T): void;
+
+  /**
+   * Render this collection component by appending to the parent element
+   * @param disableClean Boolean value to indicate whether to clean all
+   *    children elements of the parent before this component is
+   *    appended. Defaults to false.
+   */
+  render = (disableClean: boolean = false): void => {
+    // Check for cleaning
+    if (!disableClean) {
+      this.parent.innerHTML = "";
+    }
+
+    for (let model of this.collection.getAll()) {
+      this.appendComponent(model);
+    }
+  };
+}
