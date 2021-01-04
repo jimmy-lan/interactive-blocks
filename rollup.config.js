79f2be9baa845cb882ef024b90122f85c6df1383
Jimmy <jimmylanbcn@gmail.com>
2021-01-04 17:19:13 -0500

    Update package information

--- rollup.config.js
+++ rollup.config.js
@@ -19,8 +19,16 @@ const input = "src/index.ts";
 const banner = `/*! *****************************************************************************
  * interactive-blocks.js v${pkg.version}
  * ${pkg.homepage}
- * (c) ${new Date().getFullYear()} Jimmy Lan
+ * Copyright (c) ${new Date().getFullYear()} Jimmy Lan
  * Released under the MIT License
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
  ***************************************************************************** */`;
 
 export default [
